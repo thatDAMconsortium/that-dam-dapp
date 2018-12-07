bb6d9f9328eb93514c12e3b26b8ce15163494190
Zane Witherspoon <zane.witherspoon@gmail.com>
2018-12-06 16:22:20 -0800

    added solidity

---
+++ solidity/DAMToken.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract DAMToken {
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    constructor() public {
+        balanceOf[msg.sender] = 100000;              // Give the creator all initial tokens
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
+        require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
+        balanceOf[msg.sender] -= _value;                    // Subtract from the sender
+        balanceOf[_to] += _value;                           // Add the same to the recipient
+        return true;
+    }
+
+    function getBalance(address _of) public returns (uint256 balance) {
+    	return balanceOf[_of];
+    }
+}
